e3a4cbe5e49addf9cd0115d24556f0387e8cf00b
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-12 20:59:01 -0400

    Fixes

--- test/speedtest.py
+++ test/speedtest.py
@@ -4,13 +4,17 @@ import re
 def main():
     output = sys.stdin.read()
 
-    time = int(re.search(r'\d+ tests? from \d+ test suites? ran. \((\d+) ms total\)', output).group(1))
+    print(output)
+
+    print("-" * 50)
+
+    time = int(re.search(r"\d+ tests? from \d+ test suites? ran. \((\d+) ms total\)", output).group(1))
 
     # current threshold: 100ms
     if time > 100:
         print(f"Error: Test program took {time} ms")
         sys.exit(1)
     else:
-        print(f"Test program took {time} ms")
+        print(f"Success: Test program took {time} ms")
 
 main()
