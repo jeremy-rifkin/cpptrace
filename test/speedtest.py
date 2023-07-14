50d806649e6f15e40a71d67eeea8cf1f3f6154e1
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-13 21:02:26 -0400

    test

--- test/speedtest.py
+++ test/speedtest.py
@@ -10,11 +10,24 @@ def main():
 
     time = int(re.search(r"\d+ tests? from \d+ test suites? ran. \((\d+) ms total\)", output).group(1))
 
-    # current threshold: 100ms
-    if time > 100:
-        print(f"Error: Test program took {time} ms")
-        sys.exit(1)
+    dwarf4 = any(["DWARF4" in arg for arg in sys.argv[1:]])
+    dwarf5 = any(["DWARF5" in arg for arg in sys.argv[1:]])
+    expect_slow = dwarf4
+
+    threshold = 100 # ms
+
+    if expect_slow:
+        if time > 100:
+            print(f"Success (expecting slow): Test program took {time} ms")
+        else:
+            print(f"Error (expecting slow): Test program took {time} ms")
+            sys.exit(1)
     else:
-        print(f"Success: Test program took {time} ms")
+        if time > 100:
+            print(f"Error: Test program took {time} ms")
+            sys.exit(1)
+        else:
+            print(f"Success: Test program took {time} ms")
+
 
 main()
