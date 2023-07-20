4b9e132346db2d1e31e26d13d3c7876041ba3351
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-20 00:12:30 -0400

    Add libgcc unwind backend

---
+++ src/unwind/unwind_with_unwind.cpp
@@ -0,0 +1,59 @@
+#ifdef CPPTRACE_UNWIND_WITH_UNWIND
+
+#include "cpptrace_unwind.hpp"
+#include "../platform/cpptrace_common.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <vector>
+
+#include <unwind.h>
+
+namespace cpptrace {
+    namespace detail {
+        struct unwind_state {
+            std::size_t skip;
+            std::size_t count = 0;
+            std::vector<void*>& vec;
+        };
+
+        _Unwind_Reason_Code unwind_callback(_Unwind_Context* context, void* arg) {
+            unwind_state& state = *static_cast<unwind_state*>(arg);
+            if(state.skip) {
+                state.skip--;
+                //if(_Unwind_GetIP(context) != uintptr_t(0)) {
+                //    return _URC_NO_REASON;
+                //} else {
+                //    return _URC_END_OF_STACK;
+                //}
+            }
+
+            assert(state.count < state.vec.size());
+            //void* ip = reinterpret_cast<void*>(_Unwind_GetIP(context));
+            int is_before_instruction = 0;
+            uintptr_t ip = _Unwind_GetIPInfo(context, &is_before_instruction);
+            if(!is_before_instruction && ip != 0) {
+                ip--;
+            }
+            if (ip == uintptr_t(0) || state.count == state.vec.size()) {
+                return _URC_END_OF_STACK;
+            } else {
+                state.vec[state.count++] = (void*)ip;
+                return _URC_NO_REASON;
+            }
+        }
+
+        CPPTRACE_FORCE_NO_INLINE
+        std::vector<void*> capture_frames(size_t skip) {
+            std::vector<void*> frames(hard_max_frames + skip, nullptr);
+            unwind_state state{skip, 0, frames};
+            _Unwind_Backtrace(unwind_callback, &state);
+            frames.resize(state.count);
+            frames.shrink_to_fit();
+            return frames;
+        }
+    }
+}
+
+#endif
