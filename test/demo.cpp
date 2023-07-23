84b7a9af35949c859098ccd853b94f0a8d83fb46
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-23 15:58:58 -0400

    Update demo program

--- test/demo.cpp
+++ test/demo.cpp
@@ -9,42 +9,27 @@ void trace() {
     cpptrace::print_trace();
 }
 
-int x;
-
 void foo(int n) {
     if(n == 0) {
-        x = 0;
         trace();
-        x = 0;
     } else {
-        x = 0;
         foo(n - 1);
-        x = 0;
     }
 }
 
 template<typename... Args>
 void foo(int x, Args... args) {
-    x = 0;
     foo(args...);
-    x = 0;
 }
 
 void function_two(int, float) {
-    x = 0;
     foo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-    x = 0;
 }
 
 void function_one(int) {
-    x = 0;
     function_two(0, 0);
-    x = 0;
 }
 
 int main() {
-    x = 0;
     function_one(0);
-    x = 0;
 }
-
