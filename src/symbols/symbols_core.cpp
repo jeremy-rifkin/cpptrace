db4f67d352c58eaeef7fd3ea3e07c588615209ee
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-09-11 12:12:01 -0400

    Lint fixes

--- src/symbols/symbols_core.cpp
+++ src/symbols/symbols_core.cpp
@@ -4,40 +4,9 @@
 
 namespace cpptrace {
     namespace detail {
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_LIBBACKTRACE
-        namespace libbacktrace {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_LIBDWARF
-        namespace libdwarf {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_LIBDL
-        namespace libdl {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_ADDR2LINE
-        namespace addr2line {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_DBGHELP
-        namespace dbghelp {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-        #ifdef CPPTRACE_GET_SYMBOLS_WITH_NOTHING
-        namespace nothing {
-            std::vector<stacktrace_frame> resolve_frames(const std::vector<void*>& frames);
-        }
-        #endif
-
         void apply_trace(
             std::vector<stacktrace_frame>& result,
-            const std::vector<stacktrace_frame>& trace
+            std::vector<stacktrace_frame>&& trace
         ) {
             for(std::size_t i = 0; i < result.size(); i++) {
                 if(result[i].address == 0) {
