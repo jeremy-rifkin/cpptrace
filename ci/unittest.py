73412e72879308e9b2e5550975e2d39e74f454ca
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-07-12 23:24:06 -0500

    Oops

--- ci/unittest.py
+++ ci/unittest.py
@@ -35,7 +35,6 @@ def build(runner: MatrixRunner):
             f"-DCPPTRACE_USE_EXTERNAL_LIBDWARF=On",
             f"-DCPPTRACE_USE_EXTERNAL_ZSTD=On",
             f"-DCPPTRACE_USE_EXTERNAL_GTEST=On",
-            f"-DCMAKE_INSTALL_PREFIX=~/tmp/foo",
         ]
         return runner.run_command(*args) and runner.run_command("ninja")
     elif platform.system() == "Darwin":
