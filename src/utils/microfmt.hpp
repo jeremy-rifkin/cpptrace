4e9f0da95fa930fd15c5361225dae117ec715421
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-09-12 21:06:12 -0500

    Nest microfmt in the cpptrace namespace due to conditionally-enabled C++17 behavior causing ODR issues, related to https://github.com/jeremy-rifkin/libassert/issues/103

--- src/utils/microfmt.hpp
+++ src/utils/microfmt.hpp
@@ -17,6 +17,7 @@
 // https://github.com/jeremy-rifkin/microfmt
 // Format: {[align][width][:[fill][base]]}  # width: number or {}
 
+namespace cpptrace {
 namespace microfmt {
     namespace detail {
         inline std::uint64_t clz(std::uint64_t value) {
@@ -300,6 +301,7 @@ namespace microfmt {
         auto str = format(fmt, args...);
         fwrite(str.data(), 1, str.size(), stream);
     }
+}
 }
 
 #endif
