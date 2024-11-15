88df27aa32552613de4144155ebbea9f382d50e0
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-11-14 21:38:49 -0600

    Disable a unittest config until a libdwarf bug is fixed

--- ci/unittest.py
+++ ci/unittest.py
@@ -140,6 +140,12 @@ def run_linux_matrix():
                 "split_dwarf": "ON",
                 "dwarf_version": "5",
             },
+            {
+                # disabled until https://github.com/davea42/libdwarf-code/issues/267 is fixed
+                "compiler": "clang++-18",
+                "split_dwarf": "ON",
+                "dwarf_version": "5",
+            },
         ]
     ).run(build_and_test)
 
