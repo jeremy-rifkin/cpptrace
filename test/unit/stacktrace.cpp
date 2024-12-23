6919d1e9486917f4e4cdee72c2abd9d8911339f4
Sergei Zimmerman <145775305+xokdvium@users.noreply.github.com>
2024-12-23 13:51:00 +0300

    test(stacktrace): make CPPTRACE_FORCE_INLINE static
    This fixes compilation error:
    
    > error: inlining failed in call to 'always_inline' 'int stacktrace_inline_resolution_2(std::vector<int>&)': function body can be overwritten at link time

--- test/unit/stacktrace.cpp
+++ test/unit/stacktrace.cpp
@@ -11,7 +11,7 @@ using namespace std::literals;
 #ifdef _MSC_VER
  #define CPPTRACE_FORCE_INLINE [[msvc::flatten]]
 #else
- #define CPPTRACE_FORCE_INLINE [[gnu::always_inline]]
+ #define CPPTRACE_FORCE_INLINE [[gnu::always_inline]] static
 #endif
 
 
