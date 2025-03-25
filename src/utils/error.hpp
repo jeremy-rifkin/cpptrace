7c4fcc7a094cb379745df8a55d6fcc987f8c92e2
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-03-24 23:11:27 -0500

    Split error.hpp into source/header and simplify implementation

--- src/utils/error.hpp
+++ src/utils/error.hpp
@@ -19,7 +19,7 @@ namespace detail {
     class internal_error : public std::exception {
         std::string msg;
     public:
-        internal_error(std::string message) : msg("Cpptrace internal error: " + std::move(message)) {}
+        internal_error(std::string message);
         template<typename... Args>
         internal_error(const char* format, Args&&... args) : internal_error(microfmt::format(format, args...)) {}
         const char* what() const noexcept override {
@@ -45,52 +45,19 @@ namespace detail {
         panic,
     };
 
-    constexpr const char* assert_actions[] = {"assertion", "verification", "panic"};
-    constexpr const char* assert_names[] = {"ASSERT", "VERIFY", "PANIC"};
-
-    [[noreturn]] inline void assert_fail(
+    [[noreturn]] void assert_fail(
         assert_type type,
         const char* expression,
         const char* signature,
         source_location location,
         const char* message
-    ) {
-        const char* action = assert_actions[static_cast<std::underlying_type<assert_type>::type>(type)];
-        const char* name   = assert_names[static_cast<std::underlying_type<assert_type>::type>(type)];
-        if(message == nullptr) {
-            throw internal_error(
-                "Cpptrace {} failed at {}:{}: {}\n"
-                "    {}({});\n",
-                action, location.file, location.line, signature,
-                name, expression
-            );
-        } else {
-            throw internal_error(
-                "Cpptrace {} failed at {}:{}: {}: {}\n"
-                "    {}({});\n",
-                action, location.file, location.line, signature, message,
-                name, expression
-            );
-        }
-    }
+    );
 
-    [[noreturn]] inline void panic(
+    [[noreturn]] void panic(
         const char* signature,
         source_location location,
         const std::string& message = ""
-    ) {
-        if(message == "") {
-            throw internal_error(
-                "Cpptrace panic {}:{}: {}\n",
-                location.file, location.line, signature
-            );
-        } else {
-            throw internal_error(
-                "Cpptrace panic {}:{}: {}: {}\n",
-                location.file, location.line, signature, message.c_str()
-            );
-        }
-    }
+    );
 
     template<typename T>
     void nullfn() {
@@ -98,12 +65,6 @@ namespace detail {
     }
 
     #define PHONY_USE(...) (nullfn<decltype(__VA_ARGS__)>())
-
-    // Work around a compiler warning
-    template<typename T>
-    bool as_bool(T&& value) {
-        return static_cast<bool>(std::forward<T>(value));
-    }
 
     // Work around a compiler warning
     template<typename T>
@@ -118,23 +79,21 @@ namespace detail {
     // Check condition in both debug and release. std::runtime_error on failure.
     #define PANIC(...) ((::cpptrace::detail::panic)(CPPTRACE_PFUNC, CPPTRACE_CURRENT_LOCATION, ::cpptrace::detail::as_string(__VA_ARGS__)))
 
-    template<typename T>
-    void assert_impl(
-        T condition,
+    inline void assert_impl(
+        bool condition,
         const char* message,
         assert_type type,
         const char* args,
         const char* signature,
         source_location location
     ) {
-        if(!as_bool(condition)) {
+        if(!condition) {
             assert_fail(type, args, signature, location, message);
         }
     }
 
-    template<typename T>
-    void assert_impl(
-        T condition,
+    inline void assert_impl(
+        bool condition,
         assert_type type,
         const char* args,
         const char* signature,
