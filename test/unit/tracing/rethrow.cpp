a85c3f8420580f65a661a2602587e0b704066bd9
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-04-18 18:40:38 -0500

    Add another rethrow test

--- test/unit/tracing/rethrow.cpp
+++ test/unit/tracing/rethrow.cpp
@@ -142,3 +142,85 @@ TEST(Rethrow, RethrowTraceCorrect) {
         EXPECT_THAT(rethrow_trace.frames[i].symbol, testing::HasSubstr("Rethrow_RethrowTraceCorrect_Test::TestBody"));
     }
 }
+
+// NOTE: returning something and then return stacktrace_multi_3(line_numbers) * rand(); is done to prevent TCO even
+// under LTO https://github.com/jeremy-rifkin/cpptrace/issues/179#issuecomment-2467302052
+CPPTRACE_FORCE_NO_INLINE int stacktrace_from_current_basic_3(std::vector<int>& line_numbers) {
+    static volatile int lto_guard; lto_guard = lto_guard + 1;
+    if(truthy) { // due to a MSVC warning about unreachable code
+        line_numbers.insert(line_numbers.begin(), __LINE__ + 1);
+        throw std::runtime_error("foobar");
+    }
+    return 2;
+}
+
+CPPTRACE_FORCE_NO_INLINE int stacktrace_from_current_basic_2(std::vector<int>& line_numbers) {
+    static volatile int lto_guard; lto_guard = lto_guard + 1;
+    line_numbers.insert(line_numbers.begin(), __LINE__ + 1);
+    return stacktrace_from_current_basic_3(line_numbers) * rand();
+}
+
+CPPTRACE_FORCE_NO_INLINE int stacktrace_from_current_basic_1(std::vector<int>& line_numbers) {
+    static volatile int lto_guard; lto_guard = lto_guard + 1;
+    line_numbers.insert(line_numbers.begin(), __LINE__ + 1);
+    return stacktrace_from_current_basic_2(line_numbers) * rand();
+}
+
+TEST(Rethrow, RethrowDoesntInterfereWithSubsequentTraces) {
+    std::vector<int> line_numbers;
+    std::vector<int> rethrow_line_numbers;
+    CPPTRACE_TRY {
+        // do rethrow path so a rethrow happens
+        try {
+            static volatile int tco_guard = stacktrace_from_current_rethrow_1(line_numbers, rethrow_line_numbers);
+            (void)tco_guard;
+        } catch(...) {}
+        // now do real test
+        line_numbers.clear();
+        line_numbers.insert(line_numbers.begin(), __LINE__ + 1);
+        stacktrace_from_current_basic_1(line_numbers);
+    } CPPTRACE_CATCH(const std::runtime_error& e) {
+        EXPECT_EQ(e.what(), "foobar"sv);
+        const auto& trace = cpptrace::from_current_exception();
+        ASSERT_GE(trace.frames.size(), 4);
+        auto it = std::find_if(
+            trace.frames.begin(),
+            trace.frames.end(),
+            [](const cpptrace::stacktrace_frame& frame) {
+                return frame.symbol.find("stacktrace_from_current_basic_3") != std::string::npos;
+            }
+        );
+        ASSERT_NE(it, trace.frames.end());
+        size_t i = static_cast<size_t>(it - trace.frames.begin());
+        int j = 0;
+        ASSERT_LT(i, trace.frames.size());
+        ASSERT_LT(j, line_numbers.size());
+        EXPECT_FILE(trace.frames[i].filename, "rethrow.cpp");
+        EXPECT_LINE(trace.frames[i].line.value(), line_numbers[j]);
+        EXPECT_THAT(trace.frames[i].symbol, testing::HasSubstr("stacktrace_from_current_basic_3"));
+        i++;
+        j++;
+        ASSERT_LT(i, trace.frames.size());
+        ASSERT_LT(j, line_numbers.size());
+        EXPECT_FILE(trace.frames[i].filename, "rethrow.cpp");
+        EXPECT_LINE(trace.frames[i].line.value(), line_numbers[j]);
+        EXPECT_THAT(trace.frames[i].symbol, testing::HasSubstr("stacktrace_from_current_basic_2"));
+        i++;
+        j++;
+        ASSERT_LT(i, trace.frames.size());
+        ASSERT_LT(j, line_numbers.size());
+        EXPECT_FILE(trace.frames[i].filename, "rethrow.cpp");
+        EXPECT_LINE(trace.frames[i].line.value(), line_numbers[j]);
+        EXPECT_THAT(trace.frames[i].symbol, testing::HasSubstr("stacktrace_from_current_basic_1"));
+        i++;
+        j++;
+        ASSERT_LT(i, trace.frames.size());
+        ASSERT_LT(j, line_numbers.size());
+        EXPECT_FILE(trace.frames[i].filename, "rethrow.cpp");
+        EXPECT_LINE(trace.frames[i].line.value(), line_numbers[j]);
+        EXPECT_THAT(
+            trace.frames[i].symbol,
+            testing::HasSubstr("Rethrow_RethrowDoesntInterfereWithSubsequentTraces_Test::TestBody")
+        );
+    }
+}
