05c0ef6a8395cd961c73c6f0f68ca4a2ef665ded
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-04-18 19:05:57 -0500

    Fix for MSVC

--- test/unit/tracing/rethrow.cpp
+++ test/unit/tracing/rethrow.cpp
@@ -39,6 +39,7 @@ int stacktrace_from_current_rethrow_2(std::vector<int>& line_numbers, std::vecto
         rethrow_line_numbers.insert(rethrow_line_numbers.begin(), __LINE__ + 1);
         cpptrace::rethrow();
     }
+    CPPTRACE_UNREACHABLE(); // unfortunately needed under MSVC
 }
 
 CPPTRACE_FORCE_NO_INLINE
