bb826ed05c345977e52ca23e51f4942cc82142e6
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-04-20 14:49:36 -0500

    Update dwarf_options.cpp

--- src/symbols/dwarf/dwarf_options.cpp
+++ src/symbols/dwarf/dwarf_options.cpp
@@ -5,20 +5,22 @@
 #include <atomic>
 
 namespace cpptrace {
-    namespace internal {
-        std::atomic<nullable<std::size_t>> dwarf_resolver_line_table_cache_size{nullable<std::size_t>::null()};
-        std::atomic<bool> dwarf_resolver_disable_aranges{false};
+namespace internal {
+    std::atomic<nullable<std::size_t>> dwarf_resolver_line_table_cache_size{nullable<std::size_t>::null()};
+    std::atomic<bool> dwarf_resolver_disable_aranges{false};
 
-        optional<std::size_t> get_dwarf_resolver_line_table_cache_size() {
-            auto max_entries = dwarf_resolver_line_table_cache_size.load();
-            return max_entries.has_value() ? optional<std::size_t>(max_entries.value()) : nullopt;
-        }
-
-        bool get_dwarf_resolver_disable_aranges() {
-            return dwarf_resolver_disable_aranges.load();
-        }
+    optional<std::size_t> get_dwarf_resolver_line_table_cache_size() {
+        auto max_entries = dwarf_resolver_line_table_cache_size.load();
+        return max_entries.has_value() ? optional<std::size_t>(max_entries.value()) : nullopt;
     }
 
+    bool get_dwarf_resolver_disable_aranges() {
+        return dwarf_resolver_disable_aranges.load();
+    }
+}
+}
+
+CPPTRACE_BEGIN_NAMESPACE
     namespace experimental {
         void set_dwarf_resolver_line_table_cache_size(nullable<std::size_t> max_entries) {
             internal::dwarf_resolver_line_table_cache_size.store(max_entries);
@@ -28,4 +30,4 @@ namespace cpptrace {
             internal::dwarf_resolver_disable_aranges.store(disable);
         }
     }
-}
+CPPTRACE_END_NAMESPACE
