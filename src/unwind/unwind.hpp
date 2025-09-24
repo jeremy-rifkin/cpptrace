71a491b4d44a862928ffc30d3f75caa134c476e3
Pavol Gono <28236775+gpalino@users.noreply.github.com>
2025-09-25 01:17:41 +0200

    Removed windows compilation warning: 'WIN32_LEAN_AND_MEAN' redefined (#288)
    Removed this warning:
    src/unwind/unwind.hpp:10:10: warning: 'WIN32_LEAN_AND_MEAN' redefined
    Similar background like in
    https://github.com/jeremy-rifkin/cpptrace/pull/186

--- src/unwind/unwind.hpp
+++ src/unwind/unwind.hpp
@@ -7,7 +7,9 @@
 #include <vector>
 
 #ifdef CPPTRACE_UNWIND_WITH_DBGHELP
- #define WIN32_LEAN_AND_MEAN
+ #ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+ #endif
  #include <windows.h>
 #endif
 
