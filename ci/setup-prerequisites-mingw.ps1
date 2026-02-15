4b94ca34726152c34e4fb20a05d2c6b381344dce
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-02-14 23:36:55 -0600

    Setup CI caching (#309)
    This PR speeds up CI by caching dependencies and using ccache (which is
    persisted across runs in addition to significantly helping within a run)

--- ci/setup-prerequisites-mingw.ps1
+++ ci/setup-prerequisites-mingw.ps1
@@ -1,26 +1,34 @@
-mkdir zstd
-cd zstd
-git init
-git remote add origin https://github.com/facebook/zstd.git
-git fetch --depth 1 origin 63779c798237346c2b245c546c40b72a5a5913fe # 1.5.5
-git checkout FETCH_HEAD
-cd build/cmake
-mkdir build
-cd build
-cmake .. -DZSTD_BUILD_SHARED=On -DZSTD_BUILD_SHARED=Off -DZSTD_LEGACY_SUPPORT=Off -DZSTD_BUILD_PROGRAMS=Off -DZSTD_BUILD_CONTRIB=Off -DZSTD_BUILD_TESTS=Off -G"Unix Makefiles"
-make -j
+if (-not (Test-Path "zstd/build/cmake/build/lib/libzstd.a")) {
+    mkdir zstd
+    cd zstd
+    git init
+    git remote add origin https://github.com/facebook/zstd.git
+    git fetch --depth 1 origin 63779c798237346c2b245c546c40b72a5a5913fe # 1.5.5
+    git checkout FETCH_HEAD
+    cd build/cmake
+    mkdir build
+    cd build
+    cmake .. -DZSTD_BUILD_SHARED=On -DZSTD_BUILD_SHARED=Off -DZSTD_LEGACY_SUPPORT=Off -DZSTD_BUILD_PROGRAMS=Off -DZSTD_BUILD_CONTRIB=Off -DZSTD_BUILD_TESTS=Off -G"Unix Makefiles"
+    make -j
+    cd ../../../..
+}
+cd zstd/build/cmake/build
 make install
-
 cd ../../../..
 
-mkdir libdwarf
-cd libdwarf
-git init
-git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
-git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
-git checkout FETCH_HEAD
-mkdir build
-cd build
-cmake .. -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE -G"Unix Makefiles"
-make -j
+if (-not (Test-Path "libdwarf/build/src/lib/libdwarf/libdwarf.a")) {
+    mkdir libdwarf
+    cd libdwarf
+    git init
+    git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
+    git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
+    git checkout FETCH_HEAD
+    mkdir build
+    cd build
+    cmake .. -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE -G"Unix Makefiles"
+    make -j
+    cd ../..
+}
+cd libdwarf/build
 make install
+cd ../..
--- ci/setup-prerequisites-unittest.sh
+++ ci/setup-prerequisites-unittest.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-mkdir zstd
-cd zstd
-git init
-git remote add origin https://github.com/facebook/zstd.git
-git fetch --depth 1 origin 794ea1b0afca0f020f4e57b6732332231fb23c70 # 1.5.6
-git checkout FETCH_HEAD
-make -j
-sudo make install
+if [ ! -f zstd/lib/libzstd.a ]; then
+    mkdir zstd
+    cd zstd
+    git init
+    git remote add origin https://github.com/facebook/zstd.git
+    git fetch --depth 1 origin 794ea1b0afca0f020f4e57b6732332231fb23c70 # 1.5.6
+    git checkout FETCH_HEAD
+    make -j
+    cd ..
+fi
+cd zstd && sudo make install && cd ..
 
-cd ..
+if [ ! -f libdwarf/build/src/lib/libdwarf/libdwarf.a ]; then
+    mkdir libdwarf
+    cd libdwarf
+    git init
+    git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
+    git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
+    git checkout FETCH_HEAD
+    mkdir build
+    cd build
+    cmake .. -GNinja -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE
+    cd ../..
+fi
+cd libdwarf/build && sudo ninja install && cd ../..
 
-mkdir libdwarf
-cd libdwarf
-git init
-git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
-git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
-git checkout FETCH_HEAD
-mkdir build
-cd build
-cmake .. -GNinja -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE
-sudo ninja install
-
-cd ../..
-
-mkdir googletest
-cd googletest
-git init
-git remote add origin https://github.com/google/googletest.git
-git fetch --depth 1 origin f8d7d77c06936315286eb55f8de22cd23c188571
-git checkout FETCH_HEAD
-mkdir build
-cd build
-cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=/tmp/gtest_install
-sudo ninja install
-rm -rf *
-cmake .. -GNinja -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_INSTALL_PREFIX=/tmp/gtest_install_libcxx
-sudo ninja install
+if [ ! -d /tmp/gtest_install ] || [ ! -d /tmp/gtest_install_libcxx ]; then
+    mkdir googletest
+    cd googletest
+    git init
+    git remote add origin https://github.com/google/googletest.git
+    git fetch --depth 1 origin f8d7d77c06936315286eb55f8de22cd23c188571
+    git checkout FETCH_HEAD
+    mkdir build
+    cd build
+    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=/tmp/gtest_install
+    sudo ninja install
+    rm -rf *
+    cmake .. -GNinja -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_INSTALL_PREFIX=/tmp/gtest_install_libcxx
+    sudo ninja install
+    cd ../..
+fi
