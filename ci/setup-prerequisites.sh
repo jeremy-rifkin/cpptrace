5411c864ae24513bbabfcd72175e8926e5b71b57
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-02-14 18:43:30 -0600

    Add ci prerequisite caching

--- ci/setup-prerequisites.sh
+++ ci/setup-prerequisites.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-sudo apt install libgtest-dev
+sudo apt install libgtest-dev 2>/dev/null || true
 
-mkdir zstd
-cd zstd
-git init
-git remote add origin https://github.com/facebook/zstd.git
-git fetch --depth 1 origin 794ea1b0afca0f020f4e57b6732332231fb23c70 # 1.5.6
-git checkout FETCH_HEAD
-make -j
-sudo make install
+if [ ! -f zstd/lib/libzstd.a ]; then
+    mkdir zstd
+    cd zstd
+    git init
+    git remote add origin https://github.com/facebook/zstd.git
+    git fetch --depth 1 origin 794ea1b0afca0f020f4e57b6732332231fb23c70 # 1.5.6
+    git checkout FETCH_HEAD
+    make -j
+    cd ..
+fi
+cd zstd && sudo make install && cd ..
 
-cd ..
-
-mkdir libdwarf
-cd libdwarf
-git init
-git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
-git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
-git checkout FETCH_HEAD
-mkdir build
-cd build
-cmake .. -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE
-make -j
-sudo make install
+if [ ! -f libdwarf/build/src/lib/libdwarf/libdwarf.a ]; then
+    mkdir libdwarf
+    cd libdwarf
+    git init
+    git remote add origin https://github.com/jeremy-rifkin/libdwarf-lite.git
+    git fetch --depth 1 origin 5dfb2cd2aacf2bf473e5bfea79e41289f88b3a5f # 2.1.0
+    git checkout FETCH_HEAD
+    mkdir build
+    cd build
+    cmake .. -DPIC_ALWAYS=TRUE -DBUILD_DWARFDUMP=FALSE
+    make -j
+    cd ../..
+fi
+cd libdwarf/build && sudo make install && cd ../..
