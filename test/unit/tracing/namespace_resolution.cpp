a26f1e2bb81f48c0106de1b4cdf55fd0ed5649a1
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-02-27 21:38:10 -0700

    Fix and cleanup

--- test/unit/tracing/namespace_resolution.cpp
+++ test/unit/tracing/namespace_resolution.cpp
@@ -21,7 +21,6 @@ import cpptrace;
 #endif
 
 
-// --- Non-inlined, in a namespace ---
 namespace ns_test {
     CPPTRACE_FORCE_NO_INLINE void namespaced_func() {
         static volatile int lto_guard; lto_guard = lto_guard + 1;
@@ -38,8 +37,6 @@ TEST(NamespaceResolution, BasicNamespace) {
     ns_test::namespaced_func();
 }
 
-
-// --- Nested namespace ---
 namespace ns_outer { namespace ns_inner {
     CPPTRACE_FORCE_NO_INLINE void nested_ns_func() {
         static volatile int lto_guard; lto_guard = lto_guard + 1;
@@ -56,8 +53,6 @@ TEST(NamespaceResolution, NestedNamespace) {
     ns_outer::ns_inner::nested_ns_func();
 }
 
-
-// --- Struct member ---
 struct TestStruct {
     CPPTRACE_FORCE_NO_INLINE static void struct_method() {
         static volatile int lto_guard; lto_guard = lto_guard + 1;
@@ -75,15 +70,14 @@ TEST(NamespaceResolution, StructMethod) {
 }
 
 
-// --- Inlined function in namespace (DWARF-only) ---
 #if defined(CPPTRACE_GET_SYMBOLS_WITH_LIBDWARF) && !defined(CPPTRACE_BUILD_NO_SYMBOLS)
 namespace ns_inline_test {
     CPPTRACE_FORCE_NO_INLINE int ns_inline_leaf(std::vector<int>& line_numbers) {
         static volatile int lto_guard; lto_guard = lto_guard + 1;
         line_numbers.insert(line_numbers.begin(), __LINE__ + 1);
         auto trace = cpptrace::generate_trace();
-        if(trace.frames.size() < 3) {
-            ADD_FAILURE() << "trace.frames.size() >= 3";
+        if(trace.frames.size() < 4) {
+            ADD_FAILURE() << "trace.frames.size() >= 4";
             return 2;
         }
         int i = 0;
@@ -96,6 +90,11 @@ namespace ns_inline_test {
         EXPECT_LINE(trace.frames[i].line.value(), line_numbers[i]);
         EXPECT_THAT(trace.frames[i].symbol, testing::HasSubstr("ns_inline_test::ns_inline_caller"));
         EXPECT_TRUE(trace.frames[i].is_inline);
+        i++;
+        EXPECT_FILE(trace.frames[i].filename, "namespace_resolution.cpp");
+        EXPECT_LINE(trace.frames[i].line.value(), line_numbers[i]);
+        EXPECT_THAT(trace.frames[i].symbol, testing::HasSubstr("ns_inline_entry"));
+        EXPECT_FALSE(trace.frames[i].is_inline);
         i++;
         EXPECT_FILE(trace.frames[i].filename, "namespace_resolution.cpp");
         EXPECT_LINE(trace.frames[i].line.value(), line_numbers[i]);
